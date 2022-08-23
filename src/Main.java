83c88a2fb91fc77cf9c157cdf59d14cc6d7ba78e
Макс Апаков <maksapakov@gmail.com>
2022-08-23 15:29:12 +0300

    ver.1.0

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader reader = new FileReader("text.txt");
+
+        for (Map.Entry<String, Long> entry : getSalesMap(reader).entrySet()) {
+            System.out.println(entry);
+        }
+    }
+
+    public static Map<String, Long> getSalesMap(Reader reader) {
+        Map<String, Long> map = new HashMap<>();
+        Scanner scanner = new Scanner(reader);
+        try {
+            while (scanner.hasNextLine()) {
+                map.merge(scanner.next(), scanner.nextLong(), Long::sum);
+            }
+        } catch (NoSuchElementException ex) {
+
+        }
+        return map;
+    }
+
 }
